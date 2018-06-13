6c61dd8d6cd2a13daecf1e145ccccf1e00d78502
Jackieto <jackietoyt@gmail.com>
2018-06-13 20:41:02 +0200

    Creating Player.h

---
+++ Player.h
@@ -0,0 +1,26 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+#include <string>
+class Player{
+    public:
+    Player(std::string name, int health, int damage);
+    
+    void setName(std::string name);
+    std::string getName() const;
+    
+    void setHealth(int health);
+    int getHealth() const;
+    
+    void setDamage(int damage);
+    int getDamage() const;
+    
+    
+    
+    
+private:
+    std::string name;
+    int health;
+    int damage;
+};
+#endif /* JUGADOR_H */
+
